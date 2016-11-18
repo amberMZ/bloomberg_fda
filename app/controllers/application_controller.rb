8830190406bcc6e399ffc15ae2b487dd23ad6a6b
Amber <mingyuzha@gmail.com>
2016-11-18 18:50:54 +0000

    json builder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,16 +27,3 @@ class ApplicationController < ActionController::Base
   end
   
 end
-
-# class ApplicationController < ActionController::Base
-#   # Prevent CSRF attacks by raising an exception.
-#   # For APIs, you may want to use :null_session instead.
-#   protect_from_forgery with: :exception
-
-#   after_filter :set_cors
-
-#   def set_cors
-#     headers['Access-Control-Allow-Origin'] = 'https://bruce-yu-xi.github.io'
-#     headers['Access-Control-Request-Method'] = '*'
-#   end
-# end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! product, :id, :name, :upc_code, :product_code, :sell_by_date, :image_id_id, :created_at, :updated_at
+json.extract! product, :id, :name, :upc_code, :product_code, :sell_by_date, :image_id, :created_at, :updated_at
 json.url product_url(product, format: :json)
